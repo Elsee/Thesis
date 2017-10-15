9844fd0cd1bd2a6e241c239fe82d089d1a9e2732
Elsee <svetlana_else@mail.ru>
2017-10-16 00:11:48 +0300

    Graphics for one-class SVM for identification

--- oneClassSVMidentification.py
+++ oneClassSVMidentification.py
@@ -3,10 +3,19 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 import numpy as np
 import pandas
+from scipy import interp
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from itertools import cycle
+
+#users = [1,2,3,4,5,6]
+#activities = ["Jogging", "Running", "Walking down-stairs", "Walking up-stairs", "Walking"]
+#features =  ["featuresOrig", "featuresFilt", "featuresOrigPCA40", "featuresOrigPCA57", "featuresFiltPCA40", "featuresFiltPCA57"]
 
 users = [1,2,3,4,5,6]
-activities = ["Jogging", "Running", "Walking down-stairs", "Walking up-stairs", "Walking"]
-features =  ["featuresOrig", "featuresFilt", "featuresOrigPCA40", "featuresOrigPCA57", "featuresFiltPCA40", "featuresFiltPCA57"]
+activities = ["Jogging"]
+features =  ["featuresOrig"]
 
 for feature in features:
     errTable = pandas.DataFrame()
@@ -34,6 +43,8 @@ for feature in features:
             allUsersFeatures.loc[allUsersFeatures['user'] != userNum, "attack"] = -1
             
             target = allUsersFeatures['attack']
+            target = label_binarize(target, classes=[0, 1])
+            n_classes = target.shape[1]
             
             targetUser = target[target == 1]
             outliers = target[target == -1]  
@@ -47,16 +58,49 @@ for feature in features:
             train_data, test_data, train_target, test_target = train_test_split(allUsersFeatures, target, train_size = 0.8, test_size = 0.2)  
 
             model = svm.OneClassSVM(nu=0.5, kernel='rbf', gamma=0.00005)  
-            model.fit(train_data)
+            y_score = model.fit(train_data).decision_function(test_data)
             
             preds = model.predict(train_data)  
             
             
             preds1 = model.predict(allUsersFeatures)
+            
+            from sklearn.metrics import confusion_matrix
+            cm = confusion_matrix(target, preds1)
+            
             targUs = preds1[:len(targetUser)] 
             impostors = preds1[:len(outliers)]  
-            FRR = targUs[targUs == -1].size / targUs.size * 100
-            FAR = impostors[impostors == 1].size / impostors.size * 100
+            
+            FRR = cm[1][0] / (cm[1][0] + cm[1][1])
+            FAR = cm[0][1] / (cm[0][0] + cm[0][1])
+            
+            fpr = dict()
+            tpr = dict()
+            roc_auc = dict()
+            
+            for i in range(n_classes):
+                fpr[i], tpr[i], _ = roc_curve(test_target[:, i], y_score[:, i])
+                roc_auc[i] = auc(fpr[i], tpr[i])
+            
+             # Compute micro-average ROC curve and ROC area
+            fpr["micro"], tpr["micro"], _ = roc_curve(test_target.ravel(), y_score.ravel())
+            roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+            
+            plt.figure()
+            lw = 2
+            plt.plot(fpr[0], tpr[0], color='darkorange',
+                     lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[0])
+            plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+            plt.xlim([0.0, 1.0])
+            plt.ylim([0.0, 1.05])
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('ROC curve for user : ' + str(userNum) + 'doing activity: ' + activityType + ' recieved from ' + featuresType)
+            plt.legend(loc="lower right")
+            plt.show()
+            
+#            print(FRR)
+#            print(FAR)
             
             curColumn[act][counter] = [FRR, FAR]
             counter=counter+1
@@ -77,4 +121,4 @@ for feature in features:
     last = errTable.index[-1]
     errTable = errTable.rename(index={last: 'mean'})
     
-    errTable.to_csv('D:/Study/Thesis/System/svmIdent/' + feature+ '_results.csv', index = True)
+#    errTable.to_csv('D:/Study/Thesis/System/svmIdent/' + feature+ '_results.csv', index = True)
