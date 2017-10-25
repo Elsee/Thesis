9ad37a3faef5b9544636023b814f41c23147b8af
s.blinova <s.blinova@skbkontur.ru>
2017-10-25 16:55:23 +0300

    work with data to AE

--- AutoEncoderMyData/dataProcessingForAE.py
+++ AutoEncoderMyData/dataProcessingForAE.py
@@ -1,4 +1,40 @@
 import pandas as pd
 import glob
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-filenames = glob.glob('D:/Study/Thesis/System/Dataset/User '+ str(userNum) + '/' + activityType + '*.csv')
+filenames = glob.glob('../myTrainingData/featuresOrig_*.csv')
+
+totalData = pd.DataFrame()
+    
+for item in filenames:
+    # Load current dataset
+    url = item
+    #choose only accelerometer data
+    data = pd.read_csv(url, header = 0, engine='python')
+    totalData = pd.concat([totalData, data], ignore_index=True)
+
+totalData = totalData.sort_values(['user'], ascending = 1)
+
+totalData.set_index(keys=['user'], drop=False,inplace=True)
+labels=totalData['user'].unique().tolist()
+
+usersData = {}
+usersDataLen = []
+
+for i in labels:
+     usersData["user{0}".format(i)] = totalData.loc[totalData.user==i]
+
+     usersDataLen.append(usersData["user{0}".format(i)].shape[0])
+     
+minUserSamples = min(usersDataLen)
+segments = []
+sc = StandardScaler()
+
+for i in labels:
+    usersData["user{0}".format(i)] = usersData["user{0}".format(i)].head(n=minUserSamples)
+    usersData["user{0}".format(i)] = sc.fit_transform(usersData["user{0}".format(i)])
+
+    segments.append(np.array(usersData["user{0}".format(i)][:,:-1]))
+
+labels = np.asarray(pd.get_dummies(labels), dtype = np.float32)
--- AutoEncoderMyData/myAE.py
+++ AutoEncoderMyData/myAE.py
@@ -1,13 +1,19 @@
 from keras.layers import Input, Dense, Flatten, Reshape
 from keras.models import Model
 from keras.regularizers import L1L2
+import pandas as pd
+import glob
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # this is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+encoding_dim = 960  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 lambda_l1 = 0.00001
 
 # this is our input placeholder
-input_img = Input(shape=(28, 28, 1))
+input_img = Input(shape=(3923, 57, 1))
 flat_img = Flatten()(input_img)
 # "encoded" is the encoded representation of the input
 x = Dense(encoding_dim*3, activation='relu')(flat_img)
@@ -18,8 +24,8 @@ encoded = Dense(encoding_dim, activation='linear', activity_regularizer=L1L2(lam
 input_encoded = Input(shape=(encoding_dim,))
 x = Dense(encoding_dim*2, activation='relu')(input_encoded)
 x = Dense(encoding_dim*3, activation='relu')(x)
-flat_decoded = Dense(784, activation='sigmoid')(x)
-decoded = Reshape((28, 28, 1))(flat_decoded)
+flat_decoded = Dense(223611, activation='sigmoid')(x)
+decoded = Reshape((3923, 57, 1))(flat_decoded)
 
 # this model maps an input to its encoded representation
 encoder = Model(input_img, encoded)
@@ -32,14 +38,46 @@ autoencoder = Model(input_img, decoder(encoder(input_img)))
 
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-from keras.datasets import mnist
-import numpy as np
-(x_train, _), (x_test, _) = mnist.load_data()
+filenames = glob.glob('../myTrainingData/featuresOrig_*.csv')
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test  = np.reshape(x_test,  (len(x_test),  28, 28, 1))
+totalData = pd.DataFrame()
+    
+for item in filenames:
+    # Load current dataset
+    url = item
+    #choose only accelerometer data
+    data = pd.read_csv(url, header = 0, engine='python')
+    totalData = pd.concat([totalData, data], ignore_index=True)
+
+totalData = totalData.sort_values(['user'], ascending = 1)
+
+totalData.set_index(keys=['user'], drop=False,inplace=True)
+labels=totalData['user'].unique().tolist()
+
+usersData = {}
+usersDataLen = []
+
+for i in labels:
+     usersData["user{0}".format(i)] = totalData.loc[totalData.user==i]
+
+     usersDataLen.append(usersData["user{0}".format(i)].shape[0])
+     
+minUserSamples = min(usersDataLen)
+segments = []
+sc = StandardScaler()
+
+for i in labels:
+    usersData["user{0}".format(i)] = usersData["user{0}".format(i)].head(n=minUserSamples)
+    usersData["user{0}".format(i)] = sc.fit_transform(usersData["user{0}".format(i)])
+
+    segments.append(np.array(usersData["user{0}".format(i)][:,:-1]))
+
+labels = np.asarray(pd.get_dummies(labels), dtype = np.float32)
+
+x_train, x_test, y_train, y_test = train_test_split(segments, labels, test_size=0.2)
+
+x_train = np.reshape(x_train, (len(x_train), 3923, 57, 1))
+x_test  = np.reshape(x_test,  (len(x_test),  3923, 57, 1))
 print(x_train.shape)
 print(x_test.shape)
 
@@ -52,26 +90,6 @@ test = autoencoder.fit(x_train, x_train,
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
 
-# use Matplotlib (don't ask)
-import matplotlib.pyplot as plt
-
-n = 10  # how many digits we will display
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # display original
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
 
 loss_history = test.history["loss"]
 numpy_loss_history = np.array(loss_history)
